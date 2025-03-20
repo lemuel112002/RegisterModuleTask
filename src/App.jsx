24f6da7e900a790bd0d35bac97a3168ee9c7c9eb
Danreilemuel_11 <danreilemuel19@gmail.com>
2025-03-20 15:13:06 +0800

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Register from './auth/register';
+
+export default function App() {
+
+  return (
+    <div>
+      <Register />
+    </div>
+  );
+}
